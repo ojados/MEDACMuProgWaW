1a1f260b81916003a0d9c875c00d58756a14dac6
william <wzm0002@alu.medac.es>
2024-03-10 08:36:32 +0100

    Serializar Ejército

---
+++ src/Batallas/SerializarEjercito.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Batallas;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ *
+ * @author William Zapata
+ */
+public class SerializarEjercito  {
+
+    public static void serializar(Ejercito ejercito, String archivo_ejercito) throws IOException {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(archivo_ejercito))) {
+            out.writeObject(ejercito);
+        }
+
+    }
+
+    public static Ejercito deserializar(String archivo_ejercito) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(archivo_ejercito))) {
+            return (Ejercito) in.readObject();
+        }
+    }
+
+}
