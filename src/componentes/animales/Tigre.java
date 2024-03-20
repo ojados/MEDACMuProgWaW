4f1adbbbf05652be5e2809a718ee23db510d9526
JDan <110744245+soyJDan@users.noreply.github.com>
2024-03-20 19:58:38 +0100

    Refactor code and fixed errors

--- src/componentes/animales/Tigre.java
+++ src/componentes/animales/Tigre.java
@@ -4,36 +4,34 @@
  */
 package componentes.animales;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 /**
- *
  * @author danie
  */
-public class Tigre extends Animal{
-public static final int PESO_TIGRE =5;     
-    public Tigre(){
+public class Tigre extends Animal {
+    public static final int PESO_TIGRE = 5;
+
+    public Tigre() {
         super();
-        Random random = new Random(); 
-        setMultiplicador(1.5f);      
+        setMultiplicador(1.5f);
         setPeso(PESO_TIGRE);
 
         List<Integer> atributos = generarAtributos(150);
-        setAtaque((int) Math.ceil(atributos.get(0)*getMultiplicador()));
-        setDefensa((int) Math.ceil(atributos.get(1)*getMultiplicador()));
-        setSalud((int) Math.ceil(atributos.get(2)*getMultiplicador()));          
+        setAtaque((int) Math.ceil(atributos.get(0) * getMultiplicador()));
+        setDefensa((int) Math.ceil(atributos.get(1) * getMultiplicador()));
+        setSalud((int) Math.ceil(atributos.get(2) * getMultiplicador()));
     }
-    
+
     @Override
-public String toString() {
+    public String toString() {
         return "Tigre {" +
                 "Nombre='" + this.getNombre() + '\'' +
                 ", Ataque='" + this.getAtaque() + '\'' +
                 ", Defensa='" + this.getDefensa() + '\'' +
-                ", Salud='" + this.getSalud() + '\'' +                
+                ", Salud='" + this.getSalud() + '\'' +
                 ", Peso='" + this.getPeso() + '\'' +
                 '}';
-    }       
+    }
 }
