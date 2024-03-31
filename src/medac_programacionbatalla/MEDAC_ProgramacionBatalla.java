f2c8d7c9d1c2b415c3aad4d8f669ab4ecf4254eb
JDan <110744245+soyJDan@users.noreply.github.com>
2024-03-31 21:38:08 +0200

    Refactor code

--- src/medac_programacionbatalla/MEDAC_ProgramacionBatalla.java
+++ src/medac_programacionbatalla/MEDAC_ProgramacionBatalla.java
@@ -3,22 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package medac_programacionbatalla;
+
 import batallas.Batalla;
+
 /**
- *
  * @author danie
  */
-public class MEDAC_ProgramacionBatalla{
+public class MEDAC_ProgramacionBatalla {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
 //        String test = ExploradorFicheros.leerFichero();
         Batalla batalla = new Batalla();
 
-    } }
+    }
+}
     
 
  
